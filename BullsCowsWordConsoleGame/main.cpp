4d26117110a609dd0d5592ae54b8f5d8e879933a
Anthony Gutierrez <adgutierrez@techbyte.net>
2018-01-30 08:15:02 -0800

    Section 2, Lecture 22
    Iterating - For & While Loops.

--- BullsCowsWordConsoleGame/main.cpp
+++ BullsCowsWordConsoleGame/main.cpp
@@ -4,19 +4,28 @@
 using namespace std;
 
 void PrintIntro();
-string GetGuess();
 void PrintGuess();
+string GetGuess();
 
-constexpr int WORD_LENGTH = 4;
-string Guess = "";
+string PlayerGuess = "";
+string GuessAttempt[]{ "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth" };
+
+constexpr int WORD_LENGTH = 3;
+int MaxGuess = WORD_LENGTH * 2;
+int Count = 0;
 
 		//main body of application
 int main()
 {
 	PrintIntro();
-	GetGuess();
-	PrintGuess();
 
+	for (int i = 0; i < MaxGuess; i++)
+	{
+		GetGuess();
+		PrintGuess();
+		Count = Count++;
+	}
+	
 	return 0;
 }
 		// introduce the game
@@ -26,23 +35,27 @@ void PrintIntro()
 	cout << " Welcome to Bulls and Cows, a fun word game.\n" << endl;
 	cout << " Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n" << endl;
+	cout << " Two guesses per letter for a total of " << MaxGuess;
+	cout << " guesses\n";
+	cout << endl;
 	return;
 }
 
 		// get player's guess
 string GetGuess()
 {
-	
-	cout << " Enter your guess: ";
-	getline(cin, Guess);
-	return Guess;
+	cout << endl;
+	cout << " Enter your " << GuessAttempt[Count];
+	cout << " guess?  ";
+	getline(cin, PlayerGuess);
+	return PlayerGuess;
 }
 
 		// print player's guess
 void PrintGuess()
 {
 	cout << endl;
-	cout << "   Your guess was: " << Guess << endl;
+	cout << "   Your guess was: " << PlayerGuess << endl;
 	cout << endl;
 	return;
 }
